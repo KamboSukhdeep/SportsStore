d7f507c33beda8779bddac85a7e529f8e4bbf016
kambosukhdeep <sukhdeepfor.net@gmail.com>
2016-04-23 14:58:44 +0530

    Dev

---
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+namespace SportsStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public ActionResult Index()
+        {
+            return View(repository.Products);
+        }
+        public ViewResult Edit(int productId)
+        {
+            Product product = repository.Products.FirstOrDefault(x => x.ProductID == productId);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("{0} has been saved.", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+                return View(product);
+        }
+        public ViewResult Create()
+        {
+            return View("Edit", new Product());
+        }
+    }
+}
